b277430f8b37fa5e9eed6ba8e6b901be6590fc11
Shota HAMADA <maku3ts@gmail.com>
2017-06-16 00:10:40 +0900

    Add a new project

---
+++ 2017061601_ctest/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "add.hpp"
+
+int main() {
+    std::cout << add(1, 2) << "\n";
+    return 0;
+}
---
+++ 2017061601_ctest/test.cpp
@@ -0,0 +1,10 @@
+#include "add.hpp"
+
+int main() // this fails
+{
+    if (add(1, 2) == 3) {
+        return 0;
+    } else {
+        return -1;
+    }
+}
