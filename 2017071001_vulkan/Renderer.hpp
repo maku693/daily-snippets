15266f789f867e7e38b0467bd17c793a0b3e07e7
Shota HAMADA <maku3ts@gmail.com>
2017-07-10 22:48:50 +0900

    Add new project

---
+++ 2017071001_vulkan/Renderer.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Renderer {
+
+class Context;
+class Device;
+class Pipeline;
+class RenderPass;
+class SwapChain;
+
+class DepthBuffer;
+class FrameBuffer;
+class Image;
+
+} // namespace Renderer
---
+++ 2017071001_vulkan/VulkanHelper.hpp
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <vulkan/vulkan.hpp>
+
+#include <cstdint>
+#include <cstring>
+
+namespace VulkanHelper {
+
+inline bool isInstanceLayerAvailable(const std::vector<const char*>& names)
+{
+    const auto props = vk::enumerateInstanceLayerProperties();
+
+    return std::all_of(
+        names.cbegin(), names.cend(), [&props](const char* name) {
+            return std::any_of(
+                props.cbegin(), props.cend(), [name](const auto& prop) {
+                    return std::strcmp(name, prop.layerName) == 0;
+                });
+        });
+}
+
+inline bool isInstanceExtensionAvailable(const std::vector<const char*>& names)
+{
+    const auto props = vk::enumerateInstanceExtensionProperties();
+
+    return std::all_of(
+        names.cbegin(), names.cend(), [&props](const char* name) {
+            return std::any_of(
+                props.cbegin(), props.cend(), [name](const auto& prop) {
+                    return std::strcmp(name, prop.extensionName) == 0;
+                });
+        });
+}
+
+inline vk::Extent2D createSwapChainExtent(vk::Extent2D currentExtent,
+    std::uint32_t fallbackWidth, std::uint32_t fallbackHeight)
+{
+    if (currentExtent.width != -1 && currentExtent.height != -1) {
+        return currentExtent;
+    }
+
+    return { fallbackWidth, fallbackHeight };
+}
+
+} // namespace VulkanHelper
+
