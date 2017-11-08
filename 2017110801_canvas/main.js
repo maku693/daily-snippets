196070162b7325975d492477156511ba7f300329
Shota HAMADA <maku3ts@gmail.com>
2017-11-08 23:43:30 +0900

    Add a new project

---
+++ 2017110801_canvas/main.js
@@ -0,0 +1,6 @@
+(function (window) {
+    const canvas = window.document.getElementById('c')
+    const ctx = canvas.getContext('2d')
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+})(window)
