b2ef058aed1382475e8e3648bc4fe48b9f35806a
Shota HAMADA <maku693@outlook.com>
2018-04-11 23:48:26 +0900

    Add a new file

---
+++ 2018041101.js
@@ -0,0 +1,9 @@
+const Tokens = {
+  leftParen: Symbol("Left Paren"),
+  rightParen: Symbol("Right Paren"),
+  identifier: Symbol("Identifier"),
+}
+
+function leftParen(source, position) {}
+function rightParen(source, position) {}
+function identifier(source, position) {}
