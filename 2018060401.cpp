0d19c8446c554ba48f49cdcfd36c868b0cfa53da
Shota HAMADA <maku3ts@gmail.com>
2018-06-04 23:46:17 +0900

    Add a new file

---
+++ 2018060401.cpp
@@ -0,0 +1,19 @@
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan/vulkan.hpp>
+
+#include <vector>
+
+int main() {
+  const vk::ApplicationInfo app_info{};
+  std::vector<const char *> layers{"VK_LAYER_LUNARG_standard_validation"};
+  std::vector<const char *> extensions{
+      VK_KHR_SWAPCHAIN_EXTENSION_NAME, VK_KHR_SURFACE_EXTENSION_NAME,
+      VK_KHR_WIN32_SURFACE_EXTENSION_NAME, VK_EXT_DEBUG_UTILS_EXTENSION_NAME};
+  const auto device = vk::createInstance({{},
+                                          &app_info,
+                                          layers.size(),
+                                          layers.data(),
+                                          extensions.size(),
+                                          extensions.data()});
+  device.destroy();
+}
