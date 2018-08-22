3b3545902bf85d8514ce33e70a18a705d4760d00
Shota HAMADA <maku3ts@gmail.com>
2018-08-22 23:32:30 +0900

    Add a new file

---
+++ 2018082201.js
@@ -0,0 +1,14 @@
+(() => {
+  const languages = [
+    'C',
+    'C++',
+    'C#',
+    'Go',
+    'Haskell',
+    'JavaScript',
+    'Ruby',
+    'Swift',
+  ]
+  const i = Math.floor(Math.random() * languages.length)
+  console.log(languages[i])
+})()
