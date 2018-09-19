655997328dde2692e800e940548a131d1c27acbe
Shota HAMADA <maku3ts@gmail.com>
2018-09-19 23:53:16 +0900

    Add a new file

--- 2018091901.cpp
+++ 2018091901.cpp
@@ -2,24 +2,28 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <stdexcept>
 
 struct foo {
-  int32_t i;
   char c;
   float f;
+  int32_t i;
 };
 
 int main() {
   const std::filesystem::path path{"./test"};
-  foo wfoo{42, 'a', -0.1};
+
+  foo wfoo{'a', -0.1, 42};
   std::ofstream wfile{path, std::fstream::binary};
-  wfile << wfoo.i;
-  wfile << wfoo.c;
-  wfile << wfoo.f;
+  wfile.write(reinterpret_cast<char*>(&wfoo.i), sizeof(wfoo.i));
+  wfile.write(reinterpret_cast<char*>(&wfoo.c), sizeof(wfoo.c));
+  wfile.write(reinterpret_cast<char*>(&wfoo.f), sizeof(wfoo.f));
+  wfile.close();
+
   foo rfoo{};
   std::ifstream rfile{path, std::fstream::binary};
-  rfile >> rfoo.i;
-  rfile >> rfoo.c;
-  rfile >> rfoo.f;
-  std::cout << rfoo.i << ',' << rfoo.c << ',' << rfoo.f << '\n';
+  rfile.read(reinterpret_cast<char*>(&rfoo.i), sizeof(rfoo.i));
+  rfile.read(reinterpret_cast<char*>(&rfoo.c), sizeof(rfoo.c));
+  rfile.read(reinterpret_cast<char*>(&rfoo.f), sizeof(rfoo.f));
+  std::cout << rfoo.c << ',' << rfoo.f << ',' << rfoo.i << '\n';
 }
