67ca80bb33146a047b5aebe5726a1133064ed31d
Shota HAMADA <maku693@outlook.com>
2018-10-21 23:59:04 +0900

    Add a new file

---
+++ 2018102101.js
@@ -0,0 +1,11 @@
+(() => {
+  const toggles = document.querySelectorAll('*[data-content-toggle]')
+  const elements = Array.from(document.querySelectorAll(`*[data-content-lang]`))
+  elements.reduce((groups, element) => {
+    //
+  }, [])
+  for (const toggle of toggles) {
+    toggle.addEventListener('change', e => {
+    })
+  }
+})();
