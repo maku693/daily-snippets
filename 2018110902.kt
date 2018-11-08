4c15d014702a9e5a7c6bfde492a57a610020fb93
Shota HAMADA <maku693@outlook.com>
2018-11-09 01:05:51 +0900

    Add a new file

---
+++ 2018110902.kt
@@ -0,0 +1,29 @@
+import kotlin.math.sqrt
+
+data class Vec3(var x: Double, var y: Double, var z: Double) {
+    operator fun unaryMinus() = Vec3(-x, -y, -z)
+    operator fun plus(other: Vec3) = Vec3(x + other.x, y + other.y, z + other.z)
+    operator fun minus(other: Vec3) = this + -other
+
+    infix fun scale(multiplier: Double) = Vec3(
+        x * multiplier,
+        y * multiplier,
+        z * multiplier
+    )
+    infix fun dot(other: Vec3) = x * other.x + y * other.y + z * other.z
+    infix fun cross(other: Vec3) = Vec3(
+        y * other.z - z * other.y,
+        z * other.x - x * other.z,
+        x * other.y - y * other.x
+    )
+
+    fun length2() = this dot this
+    fun length() = sqrt(length2())
+    fun normalized() = this scale 1 / length()
+}
+
+fun main() {
+    println(Vec3(1.0, 0.0, 0.0) dot Vec3(1.0, 0.0, 0.0))
+    println(Vec3(4.2, 0.0, 0.0).normalized())
+}
+
